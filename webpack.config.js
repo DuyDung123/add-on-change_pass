217d515f66254dc439e9139f47fe3cf58bdea3f4
Đinh Văn Dũng <dungdv@watermelon.vn>
2022-06-08 01:04:37 +0900

    build

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: {
+    background: "./src/background/background.js",
+    content: "./src/content.js",
+    login: "./src/popup/popup.js",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.js?/,
+  //       // include: "src",
+  //       use: {
+  //         loader: "babel-loader",
+  //         options: {
+  //           presets: ["es2015"],
+  //         },
+  //       },
+  //     },
+  //   ],
+  // },
+  // devtool: 'cheap-module-source-map',
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "public", to: "" }],
+    }),
+  ],
+};
